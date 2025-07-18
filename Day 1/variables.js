288df773a5b07e30d44651c020ed468bc53ad3d2
RootOS Studio <officialinioluwaoladipupo@gmail.com>
2025-07-18 17:36:39 +0100

    Remove Day 1 JavaScript examples and assets
    Deleted all files from the 'Day 1' directory, including JavaScript example files (variables, arrays, objects, functions) and an image asset. This may be in preparation for restructuring or cleaning up the project.

--- Day 1/variables.js
+++
@@ -1,9 +0,0 @@
-const challenge = "#0x120";
-let progress = 1;
-
-console.log(challenge, progress); // #0x120 1
-progress += 1;
-console.log(challenge, progress); // #0x120 2
-progress = "Learning JS!";
-console.log(challenge, progress); // #0x120 Learning JS!
-// challenge = "New Challenge"; // Error: cannot reassign const
